f808b30686e8829766bda31fa175b8c20055bdb1
Nadezhda Komaritckaia <komaritskaya@Nadezhdas-MacBook-Pro.local>
2020-03-02 23:14:54 +0300

    Исправления линтера

--- js/avatar.js
+++ js/avatar.js
@@ -11,7 +11,7 @@
     var fileName = file.name.toLowerCase();
 
     var matches = FILE_TYPES.some(function (it) {
-     return fileName.endsWith(it);
+      return fileName.endsWith(it);
     });
 
     if (matches) {
